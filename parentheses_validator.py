0c756439080b5b3affa375cfbb54f4027d2d3158
Dr-Code-lab <at.doc@ya.ru>
2023-03-10 21:51:38 +0000

    parentheses_validator

---
+++ parentheses_validator.py
@@ -0,0 +1,46 @@
+class Parentheses_validator():
+    def __init__(self, constructor = 'init print'):
+            self.content = constructor
+            print('>>', '\n', 'CONTENT : ',constructor)
+
+
+    def isValid(self,s: str) -> bool:
+        keys = [')', ']', '}']
+        values =['(', '[', '{']
+        parentheses = {key:value for key,value in zip(keys,values)}
+        stack_operations = []
+        for char in s:
+            if char in parentheses.values():
+                stack_operations.append(char)
+            elif (not stack_operations 
+                  or not stack_operations[-1] == parentheses.get(char)):
+                return False
+            elif stack_operations[-1] == parentheses.get(char):
+                stack_operations.pop(-1)
+        if not stack_operations:
+            return True
+        return False
+        
+
+
+    def main(self):
+        if self.isValid(self.content):
+            print('\033[92m',self.isValid(self.content), '\033[0m\n........')
+        else:
+            print('\033[91m',self.isValid(self.content), '\033[0m\n........')
+
+if __name__ == '__main__':
+
+    text = (
+            '(){}[]',                                       # True
+            ']',                                            # False
+            '(])',                                          # False
+            '{',                                            # False
+            '(}',                                           # False
+            '([{()(){[)[}{{}}{{{}}}]]}]})',                 # False
+            '(({[[[[[[{{{{{}[][()]}}}}]]]]]}]))',           # False
+            '([{[([{()()}{[[[[{}{}{}][()()()]]]]}])]}])',   # True
+             )
+             
+    for string in text:
+        Parentheses_validator(string).main()
